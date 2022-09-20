9c5a478ac59ddde1d2d2aecb966920fbdd6d830d
Mihir160 <mihir.roy266@gmail.com>
2022-09-20 13:07:10 +0600

    api data load react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 const doctors = [
   {name: 'Dr. Mahfuxz', job: 'singer'},
@@ -16,7 +17,7 @@ function App() {
         nayoks.map(nayok => <Person name={nayok}></Person>)
       }
       {
-        doctors.map(doctor => <Doctor name={doctor.name}> </Doctor>)
+        doctors.map(doctor => <Doctor name={doctor.name} job={doctor.job}> </Doctor>)
       }
 
      {/* <Person name="Rubel" nykia="mousumi"></Person>
@@ -27,12 +28,15 @@ function App() {
      <Person></Person>
      <Person></Person>
      <Person></Person> */}
-     {/* <h1>New component .YAY</h1>
+     <h1>New component .YAY</h1>
      <Friend></Friend>
      <Friend></Friend>
      <Friend></Friend>
-     <Friend></Friend> */}
+     <Friend></Friend>
+     
 
+     <Counter></Counter>
+     <ExternalUsers></ExternalUsers>
     </div>
   );
 }
@@ -50,6 +54,7 @@ function Doctor(props){
    return (
     <div className='friend'>
       <h1>{props.name}</h1>
+      <h5>{props.job}</h5>
     </div>
    )
 }
@@ -67,6 +72,49 @@ function Friend(){
     <div className='friend'>
       <h3>Name: Ajay Devgun</h3>
       <p>Job: mara mari</p>
+    </div>
+  )
+}
+
+function Counter(){
+  const [count, setCount] = useState(55)
+  const increaseCount = () => setCount(count + 1)
+  const dncreaseCount = () => setCount(count - 1)
+  //   const newCount = count + 1
+  //   setCount(newCount)
+  // }
+  return(
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={dncreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+function ExternalUsers (){
+   const [users, setUsers] = useState([])
+   useEffect( ()=>{
+     fetch('https://jsonplaceholder.typicode.com/users')
+     .then(res => res.json())
+     .then(data => setUsers(data))
+   },[])
+  return (
+    <div>
+      <h2>External User</h2>
+      {/* <p>Name: {users.name}</p> */}
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return (
+    <div style={{border: '2px solid red', margin:'20px'}}>
+      <h3>Name: {props.name}</h3>
+      <h6>Email: {props.email}</h6>
     </div>
   )
 }
